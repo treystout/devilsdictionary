56040a467a0d59ac8b9db979b89b85d7a3d514a2
Trey Stout <treystout@gmail.com>
2015-03-01 19:34:45 +0000

    using a project gutenberg copy of the devil's dictionary and a stupid simple parser for it that splits it into programmable items

---
+++ dd_parser.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""Parses the project gutenberg version of the devil's dictionary from plain
+text into a python dictionary
+"""
+import re
+
+#RE_def = re.compile(r'^([A-Z]+), (.{1,3})\.\s+(.+?)', re.S)
+RE_def = re.compile(r'^([A-Z]+?), (.{1,3})\.', re.M | re.S)
+
+d = {}
+words = []
+with open("devilsdictionary.txt") as f:
+  matches = RE_def.split(f.read())
+  for i in range(1, len(matches), 3):
+    if i > len(matches)-2:
+      break
+    word = matches[i].strip()
+    part = matches[i+1].strip()
+    definition = matches[i+2].strip()
+    d[word] = {
+        'part': part,
+        'definition': definition
+      }
+    words.append({
+        'word': word,
+        'part': part,
+        'definition': definition
+      })
+
+#for k, v in d.items():
+#  print k, "->", v['definition']
+
+for w in words:
+  print "%s\t(%s.)" % (w['word'], w['part'])
+  print "\t|||%s|||\n" % w['definition']
+
+
